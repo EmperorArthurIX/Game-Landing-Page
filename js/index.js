73117bbcec57c5635c4d94cb51b4025262cdd809
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2022-06-02 16:20:32 +0530

    Updates for JS and CSS

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector(".description").onclick = function ()
+{
+    document.querySelector(".description").innerHTML += "<h2>You have chosen to be watched. Sift or be sifted!</h2>";
+}
